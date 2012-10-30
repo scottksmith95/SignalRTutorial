cef33ce623bca5bd242c97afbc06c66228987227
Scott Smith <scottksmith95@gmail.com>
2012-10-30 16:16:41 -0700

    Initial commit

---
+++ SignalRTutorial/Hubs/TagHub.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using SignalR.Hubs;
+
+namespace SignalRTutorial.Hubs
+{
+    public class TagHub : Hub
+    {
+        static List<string> _tags = new List<string>();
+
+        static TagHub()
+        {
+            _tags.Add("c#");
+            _tags.Add(".NET");
+            _tags.Add("SignalR");
+        }
+
+        public void getTags()
+        {
+            //Call the initTags method on the calling client
+            Caller.initTags(_tags);
+        }
+
+        public void setTag(string tag)
+        {
+            //Add the new tag to the list of tags
+            _tags.Add(tag);
+
+            //Call the addTag method on all connected clients
+            Clients.addTag(tag);
+        }
+    }
+}
